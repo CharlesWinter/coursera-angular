f915916966306fc553f86197ec6d55dbaf6f8f1b
Charles Winter <Charles@Charless-iMac.fritz.box>
2016-12-10 13:41:24 +0100

    moved to top directory

---
+++ app.js
@@ -0,0 +1,45 @@
+(function () {
+'use strict';
+
+angular.module('Appication1', [])
+.controller('ListController', ListController);
+
+ListController.$inject = ['$scope'];
+
+function ListController($scope) {
+
+  $scope.parseInput = function() {
+    //Get the list from the page
+    var listToParse = $scope.inputlist;
+
+    //split the number of items on the number of commas
+
+    if(listToParse) {
+      var parsedList = listToParse.split(',');
+
+      //Need to remove all blank entries from the list
+      for(var i = 0; i < parsedList.length;i++) {
+        if(parsedList[i] == " " || parsedList[i] == "") {
+          parsedList.splice(i,1);
+          i--;
+        }
+      }
+
+      console.debug(parsedList);
+      if (parsedList.length <= 3) {
+        $scope.warningLabel = "Enjoy!";
+        $scope.warningColor = "green";
+      }
+      else {
+        $scope.warningLabel = "Too much!";
+        $scope.warningColor = "green";
+      }
+    }
+    else {
+        $scope.warningLabel = "Please enter data first";
+        $scope.warningColor = "red";
+    }
+  };
+}
+
+})();
